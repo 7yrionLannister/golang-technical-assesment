fed5bed15b6ce3918b760119bc670af38aaa14c4
Daniel Fernández <47828494+7yrionLannister@users.noreply.github.com>
2025-01-26 12:50:46 -0500

    test: weekly energy consumption report

--- app/src/service/energy_consumption.service_test.go
+++ app/src/service/energy_consumption.service_test.go
@@ -95,6 +95,70 @@ func TestGetEnergyConsumptionsMonthly_Success(t *testing.T) {
 	mockDB.AssertExpectations(t)
 }
 
+func TestGetEnergyConsumptionsWeekly_Success(t *testing.T) {
+	// For
+	// Input
+	metersIds := []uint{0, 1}
+	startDate, _ := time.Parse("2006-01-02", "2025-06-01")
+	endDate, _ := time.Parse("2006-01-02", "2025-06-15")
+
+	// Expect
+	// Database state
+	data := make([]model.EnergyConsumption, 4)
+	for i := range 4 {
+		createdAt := startDate
+		if i%2 == 0 {
+			// even index records the next week
+			createdAt = createdAt.AddDate(0, 0, 7)
+		}
+		randomId, _ := uuid.NewRandom()
+		data[i] = model.EnergyConsumption{
+			Id:          randomId,
+			DeviceId:    uint(i % 2), // 0 or 1
+			Consumption: float64(i*10 + 1),
+			CreatedAt:   createdAt,
+		}
+	}
+
+	// When
+	mockDB.On("Find", mock.Anything, "device_id = (?) AND created_at BETWEEN ? AND ?", []any{metersIds[1], startDate, startDate.AddDate(0, 0, 7)}).
+		// Then
+		Return([]model.EnergyConsumption{data[1], data[3]}, nil)
+	mockDB.On("Find", mock.Anything, "device_id = (?) AND created_at BETWEEN ? AND ?", []any{metersIds[0], startDate.AddDate(0, 0, 7), endDate}).
+		// Then
+		Return([]model.EnergyConsumption{data[0], data[2]}, nil)
+	mockDB.On("Find", mock.Anything, "device_id = (?) AND created_at BETWEEN ? AND ?", []any{metersIds[1], startDate.AddDate(0, 0, 7), endDate}).
+		// Then
+		Return([]model.EnergyConsumption{}, nil)
+	mockDB.On("Find", mock.Anything, "device_id = (?) AND created_at BETWEEN ? AND ?", []any{metersIds[0], startDate, startDate.AddDate(0, 0, 7)}).
+		// Then
+		Return([]model.EnergyConsumption{}, nil)
+
+	// Test
+	result, err := GetEnergyConsumptions(metersIds, startDate, endDate, "weekly")
+
+	expectedResult := []dto.EnergyConsumptionDTO{
+		{MeterId: 0, Address: mock.Anything, Active: []float64{0, 22}},
+		{MeterId: 1, Address: mock.Anything, Active: []float64{42, 0}},
+	}
+	// Assert
+	assert.NoError(t, err)
+	assert.Len(t, result.DataGraph, 2)
+	assert.Condition(t, func() bool {
+		for index, expected := range expectedResult {
+			actual := result.DataGraph[index]
+			// Ignore address in comparison
+			equal := expected.MeterId == actual.MeterId && reflect.DeepEqual(expected.Active, actual.Active)
+			if !equal {
+				return false
+			}
+		}
+		return true
+	}, "result.DataGraph is not equal to expectedResult")
+	assert.Equal(t, []string{"June 1 - June 7", "June 8 - June 14"}, result.Period)
+	mockDB.AssertExpectations(t)
+}
+
 func TestGetEnergyConsumptionsMonthly_Error(t *testing.T) {
 	// For
 	// Input
@@ -112,6 +176,31 @@ func TestGetEnergyConsumptionsMonthly_Error(t *testing.T) {
 
 	// Test
 	result, err := GetEnergyConsumptions(metersIds, startDate, endDate, "monthly")
+
+	// Assert
+	assert.Nil(t, result)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "database error")
+	mockDB.AssertExpectations(t)
+}
+
+func TestGetEnergyConsumptionsWeekly_Error(t *testing.T) {
+	// For
+	// Input
+	metersIds := []uint{0, 1}
+	startDate, _ := time.Parse("2006-01-02", "2024-02-01")
+	endDate, _ := time.Parse("2006-01-02", "2024-03-01")
+
+	// Expect
+	expectedErr := errors.New("database error")
+
+	// When
+	mockDB.On("Find", mock.Anything, mock.Anything, mock.Anything).
+		// Then
+		Return(nil, expectedErr)
+
+	// Test
+	result, err := GetEnergyConsumptions(metersIds, startDate, endDate, "weekly")
 
 	// Assert
 	assert.Nil(t, result)
